d9e997e6ff079245d0c6d7289e59cbd846c53572
Damon Benz <benzdj11@uww.edu>
2021-02-08 20:57:47 -0600

    First commit

--- 01-java-fundamentals/01-first-java-program/FirstProgram.java
+++ 01-java-fundamentals/01-first-java-program/FirstProgram.java
@@ -1,7 +1,7 @@
 public class FirstProgram{
      public static void main(String[] args) {
         System.out.println("My Name is Damon");
-        System.out.println("I am 24 years old");
+        System.out.println("I am 24 years old.");
         System.out.println("My Hometown is Green Bay, WI");
     }
 }
