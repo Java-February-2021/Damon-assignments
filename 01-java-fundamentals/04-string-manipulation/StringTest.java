7435c8686192c9a86faf814e8434e69565714cd4
Damon Benz <benzdj11@uww.edu>
2021-02-14 15:28:18 -0600

    Lesson 04

---
+++ 01-java-fundamentals/04-string-manipulation/StringTest.java
@@ -0,0 +1,27 @@
+public class StringTest {
+    public static void main(String[] args) {
+        StringManipulator manipulator = new StringManipulator();
+
+        String str = manipulator.trimAndConcat("  Hello  ", "  World  ");
+        System.out.println(str);
+
+        char letter = 'o';
+        Integer a = manipulator.getIndexOrNull("Coding", letter);
+        Integer b = manipulator.getIndexOrNull("Hello World", letter);
+        Integer c = manipulator.getIndexOrNull("Hi", letter);
+        System.out.println(a); // 1
+        System.out.println(b); // 4
+        System.out.println(c); // null
+
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+        Integer A = manipulator.getIndexOfNull(word, subString);
+        Integer B = manipulator.getIndexOfNull(word, notSubString);
+        System.out.println(a); // 2
+        System.out.println(b); // null
+
+        String words = manipulator.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(word); // eworld
+    }
+}
