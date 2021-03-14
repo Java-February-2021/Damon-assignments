bd67a2526363d3200892a9e2dac309c008fd20bf
Damon Benz <benzdj11@uww.edu>
2021-03-14 14:56:37 -0500

    humantest

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,11 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human humanfred = new Human();
+        Human humanTom = new Human();
+
+        
+        humanTom.attack(humanfred);
+
+        
+    }
+}
